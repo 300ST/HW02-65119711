9fa4607c432d6c11508186548147c3a1611ce3d0
300ST <peerapatpimloy@gmail.com>
2024-08-23 18:41:36 +0700

    main.dart

---
+++ main.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'product.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late Future<List<Product>> futureProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProducts = fetchProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'IT@WU Shop',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder<List<Product>>(
+        future: futureProducts,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ProductList(products: snapshot.data!);
+          } else if (snapshot.hasError) {
+            return Center(child: Text("${snapshot.error}"));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+
+  const ProductList({required this.products});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        final product = products[index];
+        return ListTile(
+          leading: Image.network(
+            product.image,
+            fit: BoxFit.cover,
+            width: 50,
+            height: 50,
+          ),
+          title: Text(
+            product.title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text('\$${product.price.toString()}'),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailsScreen(product: product),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class DetailsScreen extends StatefulWidget {
+  final Product product;
+
+  const DetailsScreen({required this.product});
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  late double currentRating;
+
+  @override
+  void initState() {
+    super.initState();
+    currentRating = widget.product.rating;
+  }
+
+  TextStyle get titleStyle => const TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  TextStyle get subtitleStyle => const TextStyle(
+    fontSize: 16,
+    color: Colors.grey,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail'),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 200,
+                width: double.infinity,
+                child: Image.network(
+                  widget.product.image,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(widget.product.title, style: titleStyle),
+              const SizedBox(height: 5),
+              Text('\$${widget.product.price}', style: titleStyle),
+              const SizedBox(height: 10),
+              Text('Category', style: subtitleStyle),
+              Text(widget.product.category, style: titleStyle),
+              const SizedBox(height: 10),
+              Text(widget.product.description),
+              const SizedBox(height: 10),
+              Text(
+                'Rating : $currentRating / 5 of ${widget.product.count}',
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              RatingBar.builder(
+                initialRating: currentRating,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemBuilder: (context, _) => const Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  setState(() {
+                    currentRating = rating;
+                  });
+                  print(rating);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
